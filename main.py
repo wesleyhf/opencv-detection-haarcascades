8fe76aae1d73b7f936d22c02c983168956391ba8
Wesley Francisco <wesleyhf.contato@gmail.com>
2019-03-31 02:49:59 -0300

    removing comments

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import detector
 
-# detector.image(
-#     'files/faces.jpg',
-#     'output/faces.jpg',
-#     'haarcascades/frontalface_alt.xml',
-#     1.3,
-#     2
-# );
+detector.image(
+    'files/faces.jpg',
+    'output/faces.jpg',
+    'haarcascades/frontalface_alt.xml',
+    1.3,
+    2
+);
 
-# detector.image(
-#     'files/faces2.png',
-#     'output/faces2.png',
-#     'haarcascades/frontalface_alt.xml',
-#     1.2,
-#     2
-# );
+detector.image(
+    'files/faces2.png',
+    'output/faces2.png',
+    'haarcascades/frontalface_alt.xml',
+    1.2,
+    2
+);
 
 detector.video(
     'files/chroma.avi',
@@ -22,28 +22,28 @@ detector.video(
     'haarcascades/frontalface_alt.xml',
 );
 
-# detector.video(
-#     'files/walking.avi',
-#     'output/walking.avi',
-#     'haarcascades/fullbody.xml',
-#     1.2,
-#     5
-# );
+detector.video(
+    'files/walking.avi',
+    'output/walking.avi',
+    'haarcascades/fullbody.xml',
+    1.2,
+    5
+);
 
-# detector.image(
-#     'files/cars.jpg',
-#     'output/cars.jpg',
-#     'haarcascades/car.xml',
-# );
+detector.image(
+    'files/cars.jpg',
+    'output/cars.jpg',
+    'haarcascades/car.xml',
+);
 
-# detector.video(
-#     'files/cars1.avi',
-#     'output/cars1.avi',
-#     'haarcascades/car.xml',
-# );
+detector.video(
+    'files/cars1.avi',
+    'output/cars1.avi',
+    'haarcascades/car.xml',
+);
 
-# detector.video(
-#     'files/cars2.avi',
-#     'output/cars2.avi',
-#     'haarcascades/car.xml',
-# );
+detector.video(
+    'files/cars2.avi',
+    'output/cars2.avi',
+    'haarcascades/car.xml',
+);
